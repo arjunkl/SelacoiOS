d1c4a78ed7aefc6b17ab50392223a66a352198a4
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-19 17:20:51 +0530

    feat: declare GZSelaco iOS shell bridge

---
+++ platform/ios-shell/SelacoEngineBoundary.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char *SelacoIOSGameSignature(void);
+const char *SelacoIOSEngineVersion(void);
+bool SelacoIOSQueueSelfTest(void);
+
+#ifdef __cplusplus
+}
+#endif
