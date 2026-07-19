fd8a545e29d04e01aacad9c8e4169737d91a9245
arjunkl <58782223+arjunkl@users.noreply.github.com>
2026-07-19 17:45:54 +0530

    feat: expose Vulkan metal-surface self-test

--- platform/ios-shell/SelacoVulkanBoundary.h
+++ platform/ios-shell/SelacoVulkanBoundary.h
@@ -5,6 +5,7 @@ extern "C" {
 #endif
 
 int SelacoIOSVulkanSelfTest(void);
+int SelacoIOSVulkanSurfaceSelfTest(void *metal_layer);
 unsigned int SelacoIOSVulkanCompiledVersion(void);
 
 #ifdef __cplusplus
